d3ce0499a33fd09096308e1bc63efd40239f972e
Devran Cosmo Uenal <maccosmo@gmail.com>
2024-04-05 18:59:51 +0200

    Add simple tests

--- test/test_azure_openai.rb
+++ test/test_azure_openai.rb
@@ -30,4 +30,42 @@ class TestAzureOpenAI < Minitest::Test
 
     assert_equal result, AzureOpenAI::Helper.clean_body(json)
   end
+
+  def test_stream_merger_content
+    kind = "content"
+    test_stream_merger(kind)
+  end
+
+  def test_stream_merger_tool_calls
+    kind = "tool_calls"
+    test_stream_merger(kind)
+  end
+
+  private
+
+  def test_stream_merger(kind) # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
+    input_file_path = "stream_#{kind}.jsonl"
+    reference_file_path = "stream_#{kind}_result.json"
+
+    merger = AzureOpenAI::StreamMerger.new
+
+    stream_data = File
+                  .read(File.expand_path("fixtures/#{input_file_path}", __dir__))
+                  .each_line
+                  .map { |line| JSON.parse(line) unless line.strip.empty? }
+                  .compact
+
+    stream_data.each do |stream_chunk|
+      merger.merge(stream_chunk)
+    end
+
+    merged_stream_data = merger.merged
+
+    # Ensuring choices are sorted by index for consistency
+    merged_stream_data["choices"].sort_by! { |choice| choice["index"] }
+
+    reference = JSON.parse(File.read(File.expand_path("fixtures/#{reference_file_path}", __dir__)))
+
+    assert_equal reference, merged_stream_data, "Stream merger failed for #{kind}"
+  end
 end
