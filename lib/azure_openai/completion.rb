800bcce2402dedb245fca4f6a2f4854f0aef0684
Devran Cosmo Uenal <maccosmo@gmail.com>
2024-04-24 22:39:42 +0200

    Add utf8 to content type and update gems

--- lib/azure_openai/completion.rb
+++ lib/azure_openai/completion.rb
@@ -44,7 +44,7 @@ module AzureOpenAI
 
     def request_headers
       {
-        "Content-Type" => "application/json",
+        "Content-Type" => "application/json; charset=utf-8",
         "api-key" => api_key
       }
     end
